9cfb2e75dfe3db447387d68f6921d0717f24f624
Dmytro Bokov <dimabokov5@gmail.com>
2025-11-23 00:42:50 +0000

    fix: round up temperature values in jobs.py and cast to int humidity, CO2 and PM2.5

--- app/jobs.py
+++ app/jobs.py
@@ -6,10 +6,10 @@ def store_per_hour():
     data = list(mongoDB.db.telemetry.find().sort("created_at", DESCENDING).limit(6))
     data.reverse()
 
-    avg_temperature = sum(d.get("temperature", 0) for d in data) / len(data)
-    avg_humidity = sum(d.get("humidity", 0) for d in data) / len(data)
-    avg_co2  = sum(d.get("co2", 0) for d in data) / len(data)
-    avg_pm25 = sum(d.get("pm25", 0) for d in data) / len(data)
+    avg_temperature = round(sum(d.get("temperature", 0) for d in data) / len(data), 2)
+    avg_humidity = int(sum(d.get("humidity", 0) for d in data) / len(data),2)
+    avg_co2  = int(sum(d.get("co2", 0) for d in data) / len(data))
+    avg_pm25 = int(sum(d.get("pm25", 0) for d in data) / len(data))
     
     mongoDB.db.telemetry_per_hour.insert_one({"avg_temperature": avg_temperature, 
                                               "avg_humidity" : avg_humidity,
@@ -20,10 +20,10 @@ def store_per_hour():
 def store_per_day():
     data = list(mongoDB.db.telemetry_per_hour.find().sort("created_at", DESCENDING).limit(24))
     data.reverse()
-    avg_temperature = sum(d.get("avg_temperature", 0) for d in data) / len(data)
-    avg_humidity = sum(d.get("avg_humidity", 0) for d in data) / len(data)
-    avg_co2  = sum(d.get("avg_co2", 0) for d in data) / len(data)
-    avg_pm25 = sum(d.get("avg_pm25", 0) for d in data) / len(data)
+    avg_temperature = round(sum(d.get("temperature", 0) for d in data) / len(data), 2)
+    avg_humidity = int(sum(d.get("humidity", 0) for d in data) / len(data),2)
+    avg_co2  = int(sum(d.get("co2", 0) for d in data) / len(data))
+    avg_pm25 = int(sum(d.get("pm25", 0) for d in data) / len(data))
     
     mongoDB.db.telemetry_per_day.insert_one({"avg_temperature": avg_temperature, 
                                               "avg_humidity" : avg_humidity,
